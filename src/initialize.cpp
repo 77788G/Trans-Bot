fa760da15e80a399a015956b4a19a29068006f16
Zachary Webb <ZWebb@Calvaryftl.Local>
2020-01-30 16:19:09 -0500

    angler

--- src/initialize.cpp
+++ src/initialize.cpp
@@ -4,8 +4,8 @@
 using namespace pros;
 
 void initialize() {
-  liftR.set_brake_mode(E_MOTOR_BRAKE_HOLD);
-  liftL.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+  angle.set_brake_mode(E_MOTOR_BRAKE_HOLD);
+  lift.set_brake_mode(E_MOTOR_BRAKE_HOLD);
   intakeL.set_brake_mode(E_MOTOR_BRAKE_HOLD);
   intakeR.set_brake_mode(E_MOTOR_BRAKE_HOLD);
   lcd::print(0, "THE 0-6 ROBOSLAM DREAM BABY!!!");
